12c6fbd72d32bdfeda7732d8419478df550c353e
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2025-03-17 10:50:52 +0500

    js fundamentals revision

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,13 +1,9 @@
-console.log(`js integration`)
+let amirAge = 23
+let amirEmail = `amir@gmail.com`
+let amirName = `muhammad aamir khan`
 
-const name = document.getElementById("name")
-const email = document.getElementById("email")
-const message = document.getElementById("message")
+console.log(`my name is ${amirName}, my age is ${amirAge} and my email is ${amirEmail}`)
 
-const contactForm = document.getElementById("contactForm")
-contactForm.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    console.log(name.value)
-    console.log(email.value)
-    console.log(message.value)
-})
+const displayProfile = (name, age, email, password) => {
+    return `<h1>{$name}</h1>`
+}
